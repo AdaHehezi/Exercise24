9d21479a78cbcd544e6c83ac0829ecd9e3a1ec9f
AdaHehezi <hehezi2011@gmail.com>
2023-10-01 21:47:56 -0700

    update FAQ.js

--- ecommercesitecra/src/components/FAQ.js
+++ ecommercesitecra/src/components/FAQ.js
@@ -1,120 +1,38 @@
+import React from 'react';
+import faqData from './FAQ.json'
+
 const FAQ = () => {
-
     return(
-      
-      <><h2>Frequently Asked Questions (FAQ)</h2><div className="accordion accordion-flush" id="accordionFlushExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingOne">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="true" aria-controls="flush-collapseOne">
-              1. How can I contribute to Wanderlust Worlds?
-            </button>
-          </h2>
-          <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              We welcome contributions from fellow travelers and adventurers! If you have a travel story, tips, or recommendations to share, please visit our "Contribute" page to learn how you can become a part of our community.
-            </div>
-          </div>
+      <><h2>Frequently Asked Questions (FAQ)</h2>
+      <div class="accordion accordion-flush" id="accordionFlushExample">
+        {faqData.map((faq,index)=>(
+        <div className="accordion-item" key={index}>
+        <h2 className="accordion-header" id={`flush-heading${index}`}>
+          <button
+            className="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target={`#flush-collapse${index}`}
+            aria-expanded="true"
+            aria-controls={`flush-collapse${index}`}
+          >
+            {faq.question}
+          </button>
+        </h2>
+        <div
+          id={`flush-collapse${index}`}
+          className="accordion-collapse collapse"
+          aria-labelledby={`flush-heading${index}`}
+          data-bs-parent="#accordionFlushExample"
+        >
+          <div className="accordion-body">{faq.answer}</div>
         </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingTwo">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="true" aria-controls="flush-collapseTwo">
-              2. Do you offer travel booking services or tours?
-            </button>
-          </h2>
-          <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              No, we do not provide booking services or guided tours. We focus on inspiring and providing resources for independent travelers to plan their own adventures.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingThree">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="true" aria-controls="flush-collapseThree">
-              3. How can I stay updated with Wanderlust Worlds' latest content?
-            </button>
-          </h2>
-          <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              You can subscribe to our newsletter to receive regular updates on our latest articles, travel guides, and stories. You can also follow us on social media platforms such as Instagram, Facebook, and Twitter.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingFour">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="true" aria-controls="flush-collapseFour">
-              4. Are the travel recommendations and guides up-to-date?
-            </button>
-          </h2>
-          <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              We strive to keep our content as current as possible. However, please note that travel information can change over time. We recommend double-checking details like opening hours, prices, and availability with local sources before your trip.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingFive">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="true" aria-controls="flush-collapseFive">
-              5. What kind of destinations do you cover?
-            </button>
-          </h2>
-          <div id="flush-collapseFive" className="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              We specialize in uncovering unique and off-the-beaten-path destinations. Our goal is to showcase hidden gems and diverse cultures from around the world. You'll find a wide range of destinations, from remote villages to bustling cities.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingSix">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="true" aria-controls="flush-collapseSix">
-              6. How do you ensure the authenticity of your content?
-            </button>
-          </h2>
-          <div id="flush-collapseSix" className="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              We believe in experiencing the places we write about firsthand. Our team visits and explores the destinations we feature, engages with local cultures, and shares our honest impressions to provide authentic travel experiences and recommendations.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingSeven">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="true" aria-controls="flush-collapseSeven">
-              7. How can I connect with other travelers in the Wanderlust Worlds community?
-            </button>
-          </h2>
-          <div id="flush-collapseSeven" className="accordion-collapse collapse" aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              You can join our vibrant community of travelers by participating in our forums, commenting on articles, and sharing your own travel experiences. Connect with like-minded adventurers, ask questions, and discover new perspectives on travel.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingEight">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="true" aria-controls="flush-collapseEight">
-              8. What is Wanderlust Worlds' commitment to sustainability?
-            </button>
-          </h2>
-          <div id="flush-collapseEight" className="accordion-collapse collapse" aria-labelledby="flush-headingEight" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              We are committed to responsible travel and minimizing our environmental impact. We promote eco-friendly practices, such as reducing plastic waste and supporting initiatives that protect our planet's natural wonders.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingNine">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseNine" aria-expanded="true" aria-controls="flush-collapseNine">
-              9. Is Wanderlust Worlds suitable for all ages and travel styles?
-            </button>
-          </h2>
-          <div id="flush-collapseNine" className="accordion-collapse collapse" aria-labelledby="flush-headingNine" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              Absolutely! Wanderlust Worlds is for everyone, regardless of age, background, or travel style. We celebrate diversity and aim to make travel accessible to all. Whether you're a solo traveler, a family, or a group of friends, you'll find valuable resources and inspiration here.
-            </div>
-          </div>
-        </div>
-      </div></>    
+      </div>
+    ))}
+  </div>
+</>
+);
+};
 
-      );
-    };
-    
     export default FAQ;
     
